a7499b810ed200be2c86aabca743647b017806bd
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-08-28 10:52:00 +0430

    game_store_app

---
+++ lib/tools/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backGroundColor1 = Colors.white;
+const backGroundColor2 = Color(0xFFE5E5E5);
+const redColor1 = Color(0xffb70808);
+const redColor2 = Color(0xFFFFCDD2);
