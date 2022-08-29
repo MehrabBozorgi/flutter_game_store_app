bb714d4c5b5d761c5e28b4f357ca0c7e519e6ae0
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-08-29 10:12:06 +0430

    game_store_app

---
+++ lib/tools/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const textStyle1=TextStyle(fontSize: 28,color: Colors.black,fontWeight: FontWeight.bold,letterSpacing: 1,);
+const textStyle2=TextStyle(fontSize: 22,color: Colors.black,fontWeight: FontWeight.bold,);
+const textStyle3=TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.bold,);
+const textStyle4=TextStyle(fontSize: 18,color: Colors.black,fontWeight: FontWeight.bold,);
+const textStyle5=TextStyle(fontSize: 16,color: Colors.black,fontWeight: FontWeight.bold,);
+const textStyle6=TextStyle(fontSize: 16,color: Colors.black,fontWeight: FontWeight.normal,);
+const textStyle7=TextStyle(fontSize: 14,color: Colors.black,fontWeight: FontWeight.bold,);
+///
+const textStyle8=TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w500,);
+const textStyle9=TextStyle(fontSize: 16,color: Colors.white,fontWeight: FontWeight.bold,);
+const textStyle10=TextStyle(fontSize: 14,color: Colors.white,fontWeight: FontWeight.bold,);
+//
+const textStyle11=TextStyle(fontSize: 16,color: Colors.grey,fontWeight: FontWeight.bold,);
+const textStyle12=TextStyle(fontSize: 14,color: Colors.grey,fontWeight: FontWeight.bold,);
+//
+const textStyle13=TextStyle(fontSize: 14,color: redColor1,fontWeight: FontWeight.bold,);
+const textStyle14=TextStyle(fontSize: 14,color: Colors.orange,fontWeight: FontWeight.bold,);
